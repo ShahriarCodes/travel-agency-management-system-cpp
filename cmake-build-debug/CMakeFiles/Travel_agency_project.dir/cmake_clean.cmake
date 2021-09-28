4e6dd69194614dc533bb0aea7d0775f9d6aa0a1f
shahriar100 <shahriar.hasan100@gmail.com>
2021-09-28 20:09:45 +0600

    first commit

---
+++ cmake-build-debug/CMakeFiles/Travel_agency_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Travel_agency_project.dir/main.cpp.o"
+  "Travel_agency_project"
+  "Travel_agency_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Travel_agency_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstring>
+//#include <conio.h>
+using namespace std;
+
+
+class  NewTravelTrip {
+    int invoice_id;
+    int date;
+    string trip_location;
+    string user_name;
+
+    void AddTrip();
+    void EditTrip();
+    void DeleteTrip();
+
+ };
+
+void NewTravelTrip::AddTrip() {
+
+}
+
+void NewTravelTrip::EditTrip() {
+
+}
+
+void NewTravelTrip::DeleteTrip() {
+
+}
+
+class ShowAllUsers {
+    void deleteUser();
+    void editUser();
+};
+
+
+int main() {
+    cout << "Hello, World!" << endl;
+
+
+}
