e907fbc9733ff5a03dbd2ba069cc1957324d2c52
shahriar100 <shahriar.hasan100@gmail.com>
2021-09-30 02:36:38 +0600

    completed edit trip

--- test.cpp
+++ test.cpp
@@ -70,30 +70,22 @@ public:
 
     // returns index of matched TravelTrip object
     int search() {
-        int search_no, i;
+        int search_no;
         string search_name, search_id;
         cout << "Enter -> 1: Search by name, 2: invoice_id, 3: cancel -----> ";
         cin >> search_no;
 
-        switch (search_no) {
-            case 1:
-                cout << "Enter search_name: ";
-                getline(cin, search_name);
+        if (search_no == 1) {
+            cout << "Enter search_name: ";
+            cin >> search_name;
 
-                i = 0;
-                while (i < trips.size()) {
-                    if (trips[i].user_name == search_name) {
-                        return i;
-                    }
-                    i++;
+            int i = 0;
+            while (i < trips.size()) {
+                if (trips[i].user_name == search_name) {
+                    return i;
                 }
-                break;
-
-            case 2:
-                break;
-
-            default:
-                cout << "search canceled" << endl;
+                i++;
+            }
         }
 
         // if not found return -1
@@ -101,21 +93,81 @@ public:
     }
 
     void editTrip() {
-        int index =  search();
-        if(index >= 0) {
+        int index = search();
+        if (index >= 0) {
+            cin.ignore(1, '\n');
+            cout << "Enter User Name (X to not change): ";
+            getline(cin, name);
+            if (name != "X") {
+                trips[index].user_name = name;
+            }
 
+            cout << "Enter User Address (X to not change): ";
+            getline(cin, addr);
+            if (addr != "X") {
+                trips[index].user_address = addr;
+            }
+
+            cout << "Enter User Phone No. (X to not change): ";
+            getline(cin, phone);
+            if (phone != "X") {
+                trips[index].phone_no = phone;
+            }
+
+            cout << "Enter Trip Start Location (X to not change): ";
+            getline(cin, startLoc);
+            if (startLoc != "X") {
+                trips[index].trip_start_location = startLoc;
+            }
+
+            cout << "Enter Trip End Location (X to not change): ";
+            getline(cin, endLoc);
+            if (endLoc != "X") {
+                trips[index].trip_end_location = endLoc;
+            }
+        } else {
+            cout << "user not found" << endl;
         }
     }
 
     void deleteTrip() {
-
+        int index = search();
     }
 
 
+    void printTrips() {
+        int i = 0;
+        while (i < trips.size()) {
+            cout << endl;
+            cout << "trips: " << endl;
+
+            cout << "invoice_id: " << trips[i].invoice_id << endl;
+            cout << "creation_date: " << trips[i].creation_date << endl;
+            cout << "user_name: " << trips[i].user_name << endl;
+            cout << "user_address: " << trips[i].user_address << endl;
+            cout << "phone_no: " << trips[i].phone_no << endl;
+            cout << "trip_start_location: " << trips[i].trip_start_location << endl;
+            cout << "trip_end_location: " << trips[i].trip_end_location << endl;
+
+            cout << endl;
+            i++;
+        }
+    }
 };
 
 int main() {
     NewTravelTrip newTravelTrip = NewTravelTrip();
     newTravelTrip.addTrip();
-    cout << newTravelTrip.trips.size();
+    cout << "Size of trips vector: " + to_string(newTravelTrip.trips.size()) << endl;
+    newTravelTrip.addTrip();
+    cout << "Size of trips vector: " + to_string(newTravelTrip.trips.size()) << endl;
+
+    newTravelTrip.printTrips();
+
+//    newTravelTrip.editTrip();
+
+//    vec.erase(vec.begin() + index);
+    newTravelTrip.trips.erase(newTravelTrip.trips.begin() + 0);
+    newTravelTrip.printTrips();
+
 }
